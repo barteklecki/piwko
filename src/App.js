88a26618bf139d1f551e4ddad020a02e761cc5d2
barteklecki <leckibartek@gmail.com>
2020-07-17 02:06:02 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
+
+const App = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Container component="main" className={classes.main} maxWidth="sm">
+                <Typography variant="h2" component="h1" gutterBottom>
+                Piwko! - search your favourite beer flower
+                </Typography>
+                <Typography variant="h5" component="h2" gutterBottom>
+                {'Search your favourite beer flower'}
+                {'Use search input:'}
+                </Typography>
+                <Typography variant="body1">info</Typography>
+            </Container>
+            <footer className={classes.footer}>
+                <Container maxWidth="sm">
+                <Typography variant="body1">
+                    Based on PUNK Api
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                    {'Copyright © '}
+                    <Link color="inherit" href="https://github.com/barteklecki/">
+                        GH/barteklecki
+                    </Link>{' 2020.'}
+                </Typography>
+                </Container>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
