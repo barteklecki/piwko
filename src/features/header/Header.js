245d2d686a7ecaf3049a45566438d10daba7b66e
barteklecki <leckibartek@gmail.com>
2020-07-19 17:13:51 +0200

    feat(Header): add styling to active route button

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import logo from '../../app/logo192.png';
 import Button from '@material-ui/core/Button';
-
 
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 
 const Header = ({ classes }) => {
+    const history = useHistory();
+    const location = history.location?.pathname;
 
     return (
         <header  className={classes.header} >
             <Button 
+                onClick={() => history.push('/list')}
                 className={classes.button}
-                variant="outlined" 
+                variant={location === '/list' ? 'contained' : 'outlined'}
             >
                 Beer Catalogue
             </Button>
             <img src={logo} className={classes.logo} alt="logo" />
             <Button 
+                onClick={() => history.push('/favourites')}
                 className={classes.button}
-                variant="outlined" 
+                variant={location === '/favourites' ? 'contained' : 'outlined'}
             >
                 Favourite Beers
             </Button>
